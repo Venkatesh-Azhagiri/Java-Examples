ff1a98d4fb4f136014d6ef061370c2551f64401a
venkatesh <venkatesh88a@gmail.com>
2019-08-01 23:25:19 +0530

    java8 Filter Examples

---
+++ TestPoc/src/java8Filter/Employee.java
@@ -0,0 +1,40 @@
+package java8Filter;
+
+public class Employee {
+
+	private Long employeeId;
+	private String name;
+	private Long salary;
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+
+}
