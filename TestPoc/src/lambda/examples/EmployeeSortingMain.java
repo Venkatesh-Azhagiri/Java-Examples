e0b2eb18a6e094457ebcd4c3c3ead0ce969219c5
venkatesh-buddy <venkatesh88a@gmail.com>
2020-10-14 22:17:58 +0530

    Comparator comparing Lambda

--- TestPoc/src/lambda/examples/EmployeeSortingMain.java
+++ TestPoc/src/lambda/examples/EmployeeSortingMain.java
@@ -1,6 +1,7 @@
 package lambda.examples;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,11 +11,13 @@ public class EmployeeSortingMain {
 		List<Employee> employees = new ArrayList<>();
 		createEmployees(employees);
 		//Ascending Order
-	     employees.sort((Employee e1, Employee e2)->e1.getSalary().compareTo(e2.getSalary()));
-	     employees.forEach(e->System.out.println("Ascending::"+e.toString()));
+	    // employees.sort((Employee e1, Employee e2)->e1.getSalary().compareTo(e2.getSalary()));
+	     //employees.forEach(e->System.out.println("Ascending::"+e.toString()));
 		//Descending Order
-		employees.sort((Employee e1, Employee e2)->e2.getSalary().compareTo(e1.getSalary()));
-		employees.forEach(e->System.out.println("Descending::"+e.toString()));
+		//employees.sort((Employee e1, Employee e2)->e2.getSalary().compareTo(e1.getSalary()));
+		//employees.forEach(e->System.out.println("Descending::"+e.toString()));
+		Collections.sort(employees, Comparator.comparing(Employee::getSalary));
+		employees.forEach(e->System.out.println("Ascending lambda::"+e.toString()));
 		
 
 	}
